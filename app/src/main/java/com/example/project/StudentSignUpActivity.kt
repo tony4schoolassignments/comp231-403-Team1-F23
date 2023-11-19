009099f7d23b9ca5c1d4197de51c8481c36ed397
eva1815 <109616875+eva1815@users.noreply.github.com>
2023-11-19 18:44:58 -0500

    Added sign up information in StudentSignUpActivity

--- app/src/main/java/com/example/project/StudentSignUpActivity.kt
+++ app/src/main/java/com/example/project/StudentSignUpActivity.kt
@@ -4,32 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class StudentSignUpActivity : AppCompatActivity() {
 
-    private lateinit var studentSignUpButton: Button
     private lateinit var studentUserName: EditText
     private lateinit var studentPasswordEditText: EditText
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private lateinit var studentSignUpButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_sign_up)
+        Toast.makeText(this,getString(R.string.welcome_sign_up_page), Toast.LENGTH_SHORT).show()
+
+        // Get the widgets reference from XML layout
+        val imageSignUpLogo = findViewById<ImageView>(R.id.image_sign_up_logo)
+
+        // Display an image on image view from resource
+        imageSignUpLogo.setImageResource(R.drawable.sign_up_logo)
+
+
+
+
+
+
     }
 }
