0709a17c7ac5aae2e1ed85fd0e339b1310a8ce0f
eva1815 <109616875+eva1815@users.noreply.github.com>
2023-11-19 18:47:49 -0500

    Get user's input in StudentSignUpActivity

--- app/src/main/java/com/example/project/StudentSignUpActivity.kt
+++ app/src/main/java/com/example/project/StudentSignUpActivity.kt
@@ -12,6 +12,7 @@ class StudentSignUpActivity : AppCompatActivity() {
     private lateinit var studentUserName: EditText
     private lateinit var studentPasswordEditText: EditText
     private lateinit var studentSignUpButton: Button
+    private lateinit var backToHomeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_sign_up)
@@ -23,6 +24,12 @@ class StudentSignUpActivity : AppCompatActivity() {
         // Display an image on image view from resource
         imageSignUpLogo.setImageResource(R.drawable.sign_up_logo)
 
+        //Get user's input
+        studentPasswordEditText = findViewById(R.id.edit_student_password_input)
+        studentUserName = findViewById(R.id.edit_student_username_input)
+
+        studentSignUpButton = findViewById(R.id.btn_student_sign_up_success)
+        backToHomeButton = findViewById(R.id.btn_back_to_home_page)
 
 
 
